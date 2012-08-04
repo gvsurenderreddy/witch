404b977eea6e390a6a4a7d631a56211a26161f72
Digit <digit.siljrath@googlemail.com>
2012-08-04 16:44:33 +0100

    idfk, was it white space making it skip the kernel section?

--- witchcraft2012-1-0.sh
+++ witchcraft2012-1-0.sh
@@ -576,8 +576,7 @@ echo "how would you like to get a kernel?
 g - gentoo-sources and genkernel 
 m - manual (incomplete)"
 echo
-read -p "select which option:   "
-
+read -p "select which option: "
 [ "$REPLY" == "g" ] && emerge genkernel && emerge gentoo-sources && genkernel all --menuconfig && ls /boot/kernel* /boot/initramfs* > /boot/kernelandinitinfo #FIXME
 [ "$REPLY" == "m" ] && echo "woah there cowboy, how complete do you think this script is already!?  didnt we tell you this bit was incomplete.  ...you will have to sort that out entirely yourself later then.  http://www.gentoo.org/doc/en/handbook/handbook-amd64.xml?part=1&chap=7#doc_chap3 might b handy"
 
