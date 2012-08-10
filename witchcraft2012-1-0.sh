94e20843df08e644aa1a5d5991c5e06378ba2057
Digit <digit.siljrath@googlemail.com>
2012-08-10 02:38:52 +0100

    i must be crazy starting this bedrock install stuff.

--- witchcraft2012-1-0.sh
+++ witchcraft2012-1-0.sh
@@ -1153,7 +1153,15 @@ case $BASEDISTRO in
 esac ;
 }
 
+editorselect() {
+echo "what is your prefered text editor? (type the name of it\'s executable as exists on host system):" && read -r EDITOR
+;
+}
 
+browserselect() {
+echo "what is your prefered web browser? (type the name of it\'s executable as exists on host system):" && read -r BROWSER
+;
+}
 
 ##########
 ##########
@@ -1167,9 +1175,41 @@ esac ;
 
 installbedrock() {
 METADISTRO=BEDROCK
-echo "sorry this part is just a place-holder so far.  please check out opensource.osu.edu/~paradigm/bedrock/ to garner some idea why digit is so keen to start to include bedrock ahead of funtoo and exherbo, and ahead of even getting the gentoo install completed."
+# you are going to want to make extensive use of this link: http://opensource.osu.edu/~paradigm/bedrock/1.0alpha1/install.html ; for the making of this variable.
+echo "sorry this part is just a place-holder so far.  please check out http://opensource.osu.edu/~paradigm/bedrock/ to garner some idea why digit is so keen to start to include bedrock ahead of funtoo and exherbo, and ahead of even getting the gentoo install completed."
 
-;
+echo
+sleep 2
+echo
+
+echo "you may want to review http://opensource.osu.edu/~paradigm/bedrock/1.0alpha1/install.html#Partitioning before deciding how to partition"
+driveprep
+editorselect
+
+echo "you may want to review \"http://opensource.osu.edu/~paradigm/bedrock/1.0alpha1/install.html#Creating the directory structure\" to see what's going on here."
+echo "cd /mnt/$DISTRONAME"
+cd /mnt/$DISTRONAME
+
+echo "mkdir -p bin boot/extlinux dev etc/init.d home lib/modules opt/bedrock/bin opt/bedrock/sbin opt/bedrock/etc proc root sbin sys tmp usr/src var/lib/urandom"
+mkdir -p bin boot/extlinux dev etc/init.d home lib/modules opt/bedrock/bin opt/bedrock/sbin opt/bedrock/etc proc root sbin sys tmp usr/src var/lib/urandom
+
+echo "The default permissions on most of these should be fine. However, /tmp needs to be writable by no-root users:"
+echo "chmod a+rwxt tmp"
+chmod a+rwxt tmp
+
+echo "you may want to review http://opensource.osu.edu/~paradigm/bedrock/1.0alpha1/install.html#Optional directories before answering this question:"
+echo "do you want these optional directories?"
+read -p "mkdir -p lib/firmware \#(y/n)?"
+[ "REPLY" == "y" ] && mkdir -p lib/firmware
+read -p "mkdir -p var/chroot \#(y/n/e)"
+[ "REPLY" == "y" ] && mkdir -p var/chroot
+[ "REPLY" == "e" ] && echo "where would you like to put it then?" && read $BEDCHROOTS && mkdir -p $BEDCHROOTS
+
+echo "you may want to review \"http://opensource.osu.edu/~paradigm/bedrock/1.0alpha1/install.html#Download core Bedrock Linux component sources\" for downloading of core components"
+
+
+
+echo "i tried to warn you that this bedrock section was incomplete, now look what you've done... stuck where you left off.  ... that's ok though, just follow the last link it was suggested you may have wanted to review, and carry on from there.";
 }
 
 
@@ -1196,7 +1236,8 @@ ARCH="uname -m"
 PACKAGEMANAGERNAME=portage
 
 ###editor section to be improved
-EDITOR=mcedit
+#EDITOR=mcedit
+editorselect
 echo "setting editor to $EDITOR " && sleep 1
 #EDITOR=hash mcedit 2>&- || { echo >&2 "mcedit is not installed.  how about nano..."; nano 1; }
 #echo "what is your prefered text editor?" && read -r EDITOR
