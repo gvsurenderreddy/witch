36f944b90dc2efab464507b93d1fb9c8386a3821
wei2912 <wei2912_support@hotmail.com>
2012-11-21 17:06:39 +0800

    Congrats! driveprep.sh passes the wei2912 ultra strict test.

--- procedure.d/driveprep.sh
+++ procedure.d/driveprep.sh
@@ -34,51 +34,68 @@ fi
 
 sleep 1
 echo
-echo "here's a nice little list of your drives."
+$WITCH/color.sh GREEN "here's a nice little list of your drives."
 fdisk -l
 echo
 
-echo "where ya putting your root dir? (e.g. sda3):"
-read -r ROOTDEV
-mount /dev/$ROOTDEV /mnt/$DISTRONAME
+error() { # first parameter is error message, second is fuction to execute
+    $WITCH/color.sh ERROR $1
+    $2
+}
 
-echo "you want a separate boot right? (y):"
-read
-if [ "$REPLY" == "y" ]
-then
-    if [ ! -d /mnt/$DISTRONAME/boot/$a ]
+rootdir() {
+    echo "where ya putting your root dir? (e.g. sda3) - (NOT /root, we mean /):"
+    read -r ROOTDEV
+    mount /dev/$ROOTDEV /mnt/$DISTRONAME || error "something went wrong. try again" rootdir
+}
+
+rootdir
+
+boot() {
+    echo "you want a separate boot right? (y):"
+    read
+    if [ "$REPLY" == "y" ]
+    then
+        if [ ! -d /mnt/$DISTRONAME/boot/$a ]
+        then 
+            mkdir /mnt/$DISTRONAME/boot
+        fi 
+
+        echo "where ya putting your boot dir? (e.g. sda1):"
+        read -r BOOTDEV 
+        mount /dev/$BOOTDEV /mnt/$DISTRONAME/boot || error "something went wrong. try again" boot
+    fi
+    # i wonder, if you can do "if $REPLY=y then else fi" or something like that. 
+}
+
+boot
+
+home() {
+    echo "you want a separate home too? (y):"
+    read
+
+    if [ "$REPLY" == "n" ]
     then 
-        mkdir /mnt/$DISTRONAME/boot
-    fi 
+        echo "ok your home partition will just be lumped in with root, like the stupid people use."
 
-    echo "where ya putting your boot dir? (e.g. sda1):"
-    read -r BOOTDEV 
-    mount /dev/$BOOTDEV /mnt/$DISTRONAME/boot
-fi
-# i wonder, if you can do "if $REPLY=y then else fi" or something like that. 
+    elif [ "$REPLY" != "y" ]
+    then
+        echo "i think you\'ve gone wrong ~ should probably start this section again, and go hack the script to ask this section more sensibly, in more functions, so it can loop around back to the same question when you answer wrong... you could go badger digit to sort that if you are too scared to learn how."
 
-echo "you want a separate home too? (y):"
-read
+    elif [ "$REPLY" == "y" ]
+    then
+        if [ ! -d /mnt/$DISTRONAME/home/$a ]
+        then 
+            mkdir /mnt/$DISTRONAME/home 
+        fi 
 
-if [ "$REPLY" == "n" ]
-then 
-    echo "ok your home partition will just be lumped in with root, like the stupid people use."
+    echo "where ya putting your home dir? (e.g. sda1):" 
+    read -r HOMEDEV
+    mount /dev/$HOMEDEV /mnt/$DISTRONAME/home || error "something went wrong. try again" home
+    fi
+}
 
-elif [ "$REPLY" != "y" ]
-then
-    echo "i think you\'ve gone wrong ~ should probably start this section again, and go hack the script to ask this section more sensibly, in more functions, so it can loop around back to the same question when you answer wrong... you could go badger digit to sort that if you are too scared to learn how."
-
-elif [ "$REPLY" == "y" ]
-then
-    if [ ! -d /mnt/$DISTRONAME/home/$a ]
-    then 
-        mkdir /mnt/$DISTRONAME/home 
-    fi 
-
-echo "where ya putting your home dir? (e.g. sda1):" 
-read -r HOMEDEV
-mount /dev/$HOMEDEV /mnt/$DISTRONAME/home
-fi
+home
 
 echo "drive prep complete" 
 sleep 1
