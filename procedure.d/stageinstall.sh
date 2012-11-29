640a960ec5ca6bef3b3506cd34f58443cbcc723f
Digit <digit.siljrath@googlemail.com>
2012-11-29 06:38:53 +0000

    stage extraction fixed to basic functionality for now, skipping extract.sh

--- procedure.d/stageinstall.sh
+++ procedure.d/stageinstall.sh
@@ -162,8 +162,15 @@ esac
 function extractstage3 {
 #set this so user can choose if they want verbose output
 echo "unpacking your stage3 to /mnt/$DISTRONAME/. this may take some time, please wait."
-$WITCH/extract.sh /mnt/$DISTRONAME/stage3-*.tar.bz2 || $WITCH/color.sh ERROR "argh something happened. i suppose you'll have to extract it yourself before proceeding."
-sleep 1 
+
+#method removed.  to be returned to later.
+#$WITCH/extract.sh /mnt/$DISTRONAME/stage3-*.tar.bz2 || $WITCH/color.sh ERROR "argh something happened. i suppose you'll have to extract it yourself before proceeding."
+
+# FIXME MAKEME
+#if $STAGELOC is set, use $STAGELOC as source tarball to extract to /mnt/$DISTRONAME/usr/
+
+#ultra basic:
+tar -xvjf /mnt/$DISTRONAME/portage-latest.tar.bz2 -C /mnt/$DISTRONAME/usr/
 
 }
 
