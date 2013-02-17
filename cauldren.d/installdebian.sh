5472a3ce3f2088de553b85c6c0ec5da117f145e1
Digit <digit.siljrath@googlemail.com>
2013-02-17 02:45:39 +0000

    test move of witchcraft scripts to new witchcraft dir

--- cauldren.d/installdebian.sh
+++
@@ -1,4 +0,0 @@
-METADISTRO=DEBIAN
-#hehe.  debootstrapping's close enough to a gentoo style stage three that we aught include it too.  :)  yay.
-echo "this is another hole yet to be filled in the script.  you could consider a manual debootstrap debian install. http://www.debian-administration.org/articles/426 see?"
-#possibly out of date, but a good suggestion of a place to start for adding debian-base to this script.
