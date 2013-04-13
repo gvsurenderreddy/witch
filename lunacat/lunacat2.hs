220dc5ff8daed92b028edc49090d615f2699a5ec
digitteknohippie <digit.siljrath@googlemail.com>
2013-04-14 00:42:22 +0100

    loads of bits, minor and macro.

--- lunacat/lunacat2.hs
+++ lunacat/lunacat2.hs
@@ -81,9 +81,9 @@ eval h "!bedrockwitch" = privmsg h "nothing much progressed, little beyond an in
 
 --knightnames
 eval h "!knightnames" = privmsg h "!digit !him !orbea !jony !wgreenhouse !wei2912 !haylo"
-eval h "!digit" = privmsg h "he who hath madeth the most, and founded and guided and whatnot, yet insists he's not the boss and anyone can make and release whatver they want with a witch stamp on it."
+eval h "!digit" = privmsg h "he who hath madeth the most, and founded and guided and whatnot, yet insists he's not the boss and anyone can make and release whatver they want with a witch stamp on it.  he who maketh the rowan, willow, jamella, slackwitch and others to come he says, yet no one has seen even a slackwitch but he, and neither willow nor jamella exist as anything but air."
 eval h "!him" = privmsg h "he who nameth, and sparketh offeth this whole thing, and nameth himself so ye look illiterate."
-eval h "!orbea" = privmsg h "he who is new to linux, but we always knew was leet"
+eval h "!orbea" = privmsg h "he who is new to linux, but we always knew was leet.  fun guy with an itchy cow."
 eval h "!jony" = privmsg h "he who hath made much, and is #1 witch user/developer... after digit and wei2912.  hath been around since the start."
 eval h "!wgreenhouse" = privmsg h "he who art steady and wise.  he who maketh the eagle."
 eval h "!wei2912" = privmsg h "he who hath come in late, but made great swathing changes and augments to witchcraft."
@@ -104,8 +104,7 @@ eval h "!witches" = privmsg h "!rowan !willow !jamella !zelda !kali !eagle !slac
 eval h "!witch" = privmsg h "if she's the weight of a duck, then burn her!    see !witches !bases !philosophy !who "
 eval h "witch!" = privmsg h "BURN HER!"
 
-eval h "!sites" = privmsg h "wa: http://tinyurl.com/witchlinux ... that links to mostly all the witch stuff out there... sorta.puffinux and invariability and whatever else, they all sorta fell through, in one way or another."
-
+--general about n stuff
 eval h "!coc" = privmsg h "code of the covenant:  dont ask to ask, just ask.  dont ask to do, just do, then inform.  no flooding.  no spam.  no official. chat priority: witch > freedomware > anything else. if you are here, you are a witch developer now.  make whatever you want."
 
 eval h "!history" = privmsg h "Him said to digit: you should make a distro called witch, so people could burn them [to cd].  that happened.  witch's philosophy since then is that anyone can make what they want, no central authority, office or hierachy.  it's a pure do-archy.  like wicca.  (but we're not actually witches/warlocks).  see !releasecron"
@@ -113,21 +112,37 @@ eval h "!releasecron" = privmsg h "slax-rowan-witch-alpha1, debian-rowan-witch-a
 
 eval h "!philosophy" = privmsg h "see !coc !witch !history"
 
+eval h "!sites" = privmsg h "wa: http://tinyurl.com/witchlinux ... that links to mostly all the witch stuff out there... sorta.  puffinux and invariability and whatever else, they all sorta fell through, in one way or another."
+
 --bases
 eval h "!bases" = privmsg h "it's witchcraft!  witches can be on any base!?  see !gentoo !funtoo !exherbo !debian !freebsd !slackware !slitaz !bedrock (and try them with +witch, like !debianwitch)"
-eval h "!gentoo" = privmsg h "gentoo based witches!?  yes! try out witchcraft."
-eval h "!funtoo" = privmsg h "not yet. hack it up in witchcraft."
-eval h "!exherbo" = privmsg h "not yet. hack it up in witchcraft."
-eval h "!debian" = privmsg h "vm alpha2&3, and iso alpha4 of the rowan releases, are debian based.  also pre-alpha in witchcraft, ready for you to help hack up."
-eval h "!freebsd" = privmsg h "not yet. hack it up in witchcraft."
-eval h "!slackware" = privmsg h "unreleased alpha7s.  come hack it up in witchcraft."
-eval h "!slitaz" = privmsg h "unreleased alpha7s.  come hack it up in witchcraft."
-eval h "!bedrock" = privmsg h "barely begun to be slotted in, in witchcraft.  come help hack it up."
-
-eval h "!remaster" = privmsg h "not yet. hack it up in witchcraft. see https://github.com/Digit/witch/issues/5 for ways and to discuss ways"
+eval h "!gentoo" = privmsg h "gentoo based witches!?  yes! try out !witchcraft."
+eval h "!funtoo" = privmsg h "not yet. hack it up in !witchcraft."
+eval h "!exherbo" = privmsg h "not yet. hack it up in !witchcraft."
+eval h "!debian" = privmsg h "vm alpha2&3, and iso alpha4 of the !rowan releases, are debian based.  also pre-alpha in !witchcraft, ready for you to help hack up."
+eval h "!freebsd" = privmsg h "not yet. hack it up in !witchcraft."
+eval h "!slackware" = privmsg h "unreleased alpha7s.  come hack it up in !witchcraft."
+eval h "!slitaz" = privmsg h "unreleased alpha7s.  come hack it up in !witchcraft."
+eval h "!bedrock" = privmsg h "barely begun to be slotted in, in !witchcraft.  come help hack it up."
 
 --extras
 eval h "!beards" = privmsg h "http://ompldr.org/vaTJzdw/beards3.png"
+
+--witchcraft
+eval h "!witchcraft" = privmsg h "an umbrella tool for all your witch installing, remastering, and witch sharing.  ~ in process of being written by digit and everyone else. come try out the code https://github.com/Digit/witch see also: !witchcraftidea !remastery !rewic !cauldren !witchhunt !rspm !witchcraftprogress and many more"
+eval h "!beckon" = privmsg h "come try out the code https://github.com/Digit/witch and have a stab at hacking in your own sections of it, or fixing other bits. (please try to understand what other people are going for, and colaborate wisely, and remember forking is 100% as ok as teaming on one thing.  we can each have it our own way.)"
+
+eval h "!witchcraftprogress" = privmsg h "witchcraft progress report [Sun Apr 14 00:17:09 BST 2013] (so far, gentoo base install works, some ground work for rowan witch done, and !rspm started, and !remastery up for dev discussion (spoilt for choice), and the sharing stuff is still just passing from vapour to pre-alpha)"
+
+eval h "!witchcraftidea" = privmsg h "managing base meta-distros and package selection & desktop configuration as the two separate components that comprise your os. "
+
+eval h "!remaster" = privmsg h "not yet. hack it up in witchcraft. see https://github.com/Digit/witch/issues/5 for ways and to discuss ways"
+
+eval h "!rspm" = privmsg h "one package manager [wrapper] to rule them all.  just like pacman rosetta: https://wiki.archlinux.org/index.php/Pacman_Rosetta except in a wrapper script.  ... handy, for a distro that aims to allow any metadistro base.  https://github.com/Digit/witch/issues/18 will make further scripting easier."
+
+eval h "!rewic" = privmsg h "stupid place-holder name for the witch remastery portion of witchcraft.  installation portion is called !cauldren, remaster portion is called !rewic.  https://github.com/Digit/witch/issues/18 we're spoiled for choice.  we just need to hack up."
+eval h "!cauldren" = privmsg h "clever final name for the witch installer portion of witchcraft."
+eval h "!witchhunt" = privmsg h "clever final name for the witch sharer/finder portion of witchcraft."
 
 -- eval h x | "!goog " `isPrefixOf` x = privmsg h (drop 4 x)
 eval _   _                       = return () -- ignore everything else 
