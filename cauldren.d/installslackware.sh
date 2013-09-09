52da8dbc26dfa044cefe081f0d054c6c4fa17b68
Digit <digit.siljrath@googlemail.com>
2013-09-09 07:27:28 +0200

    slackins

---
+++ cauldren.d/installslackware.sh
@@ -0,0 +1,6 @@
+#so, yeah, why hasnt anyone thrown in a pimp slack installer section yet... slack can go many ways... this portion of witchcraft will be iiiiiiinteresting.
+
+#why, just today, wgreenhouse was saying in #witchlinux that there oughta be merely some minimal tag file shenanagans, n already witches be pimp,  yus.  booya.
+
+
+## no seriously, lets hack this up.  slack in the witchcraft, not just in the whoever wherever.  :)   *looks at digit and wgreenhouse*
