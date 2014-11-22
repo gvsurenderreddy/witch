6ad5af81d57207709c296a689d0562fba26118a5
Digit <digit.siljrath@googlemail.com>
2014-11-22 18:00:55 +0000

    added reformation stub

---
+++ reformation.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+echo "this is only a stub"
+echo "the plan for this script called reformation:
+
+ \"a monolithic config stylee thing\"
+
+elaboration:
+
+like an extension of wei2912's refactor-necessitated variable saving to a file.  such that \"witches\" could even just be one config script.
+
+(i'm sure this idea was banded around often, but now there's at least this stub"
+echo "..."
+# variables format suitable for plain sh parsing.
+# easy, human readable and comprehendable.
+# defines any/all of base, packages, configs, sources, etc
+# remains addaptive addaptive addaptive, open for other user tweakings.
+# foss of course.  gnu gpl.
